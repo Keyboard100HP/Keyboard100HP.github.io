f647314667478cd9480ac9d5b2c2c24272e047fe
Create room <none>
2023-09-25 11:21:50 +0400

    Initial Commit

---
+++ Keyboard100HP/Keyboard100HPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keyboard100HPApp.swift
+//  Keyboard100HP
+//
+//  Created by Maksim on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Keyboard100HPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
